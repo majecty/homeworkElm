91c6192709ad72759996c50089489bc76bb65017
ParkJuhyeong <majecty@gmail.com>
2015-10-05 23:08:56 +0900

    Add darwtree initial state.

---
+++ DrawTrees.elm
@@ -0,0 +1,52 @@
+module DrawTrees where
+
+import List exposing ((::))
+import List
+import Maybe
+import ListsAndTrees exposing (..)
+
+import Color
+import Signal
+import Window
+import Mouse
+import Text as T
+import Graphics.Element as E
+import Graphics.Collage as C
+
+type alias SampleListState a = { original: List a, remain: List a }
+
+getNextElement : SampleListState a -> (Maybe a, SampleListState a )
+getNextElement state = case state.remain of
+  [] ->
+    let firstOfOriginal = List.head state.original in
+    let newState = { state | remain <- state.original } in
+      (firstOfOriginal, newState)
+  _ ->
+    let firstOfOriginal = List.head state.remain in
+    let newRemain = Maybe.withDefault [] <| List.tail state.remain in
+    let newState = { state | remain <- newRemain } in
+      (firstOfOriginal, newState)
+
+sampleState : Signal b -> List a -> Signal (Maybe a, SampleListState a)
+sampleState source list =
+  let initialState = getNextElement { original = list, remain = list } in
+  Signal.foldp (\_ (_, prevState) -> getNextElement prevState) initialState source
+
+sampleListOn : Signal b -> List a -> Signal a
+sampleListOn source xs =
+  let valueAndState = sampleState source xs in
+  Signal.map (\(Just value, state) -> value) valueAndState
+
+view : (Int,Int) -> Tree -> E.Element
+view _ _ =
+  -- TODO
+  E.spacer 0 0
+
+signalTree : Signal Tree
+signalTree =
+  -- TODO
+  Signal.constant Empty
+
+main : Signal E.Element
+main =
+  Signal.map2 view Window.dimensions signalTree
