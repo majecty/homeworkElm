2e510f9c1666eee3e50fb90f0add4dab881d5838
ParkJuhyeong <majecty@gmail.com>
2015-11-03 21:54:25 +0900

    Compiler forever run.

--- BHeaps.elm
+++ BHeaps.elm
@@ -1,7 +1,7 @@
 module BHeaps
   (Heap, empty, isEmpty, findMin, insert, deleteMin, merge) where
 
-import List ((::))
+import List exposing ((::))
 import List
 
 type Tree = Node Int (List Tree)
@@ -21,15 +21,38 @@ empty = WrapHeap []
 isEmpty : Heap -> Bool
 isEmpty h = h == empty
 
+link : (Rank, Tree) -> (Rank, Tree) -> (Rank, Tree)
+link (r, Node x1 ts1 as t1) (_, Node x2 ts2 as t2) =
+  if | x1 <= x2 -> (r + 1, Node x1 (t2::ts1))
+     | otherwise -> (r + 1, Node x2 (t1::ts2))
+
+insertTree : (Rank, Tree) -> InternalHeap -> InternalHeap
+insertTree (rank, tree) internalHeap = case internalHeap of
+  [] -> [(rank, tree)]
+
+  (rank', tree')::ts' -> if
+    | rank < rank' -> (rank, tree) :: internalHeap
+    | rank == rank' -> insertTree (link (rank, tree) (rank', tree')) ts'
+
 insert : Int -> Heap -> Heap
-insert x h =
-  -- TODO
-  h
+insert x (WrapHeap internalHeap) =
+  WrapHeap <| insertTree (0, Node x []) internalHeap
+
+merge_ : InternalHeap -> InternalHeap -> InternalHeap
+merge_ ts1 ts2 = case (ts1, ts2) of
+  ([], _) -> ts2
+  (_, []) -> ts1
+  ((r1, t1)::ts1', (r2, t2)::ts2') ->
+    if | r1 < r2 -> (r1, t1) :: merge_ ts1' ts2
+       | r1 > r2 -> (r2, t2) :: merge_ ts2' ts1
+       | otherwise ->
+          let newElem = (r1, (link t1 t2))
+          in
+           insertTree newElem (merge_ ts1' ts2')
 
 merge : Heap -> Heap -> Heap
-merge h1 h2 =
-  -- TODO
-  h1
+merge (WrapHeap h1) (WrapHeap h2) =
+  WrapHeap <| merge_ h1 h2
 
 findMin : Heap -> Maybe Int
 findMin h =
