8830ceee0214eb19871ee70b9d68dbecfe1da7ac
ParkJuhyeong <majecty@gmail.com>
2015-11-09 00:28:07 +0900

    Make polymorphic versino.

--- ExplicitMin.elm
+++ ExplicitMin.elm
@@ -1,5 +1,5 @@
-module ExplicitMin
-  (Heap, empty, isEmpty, findMin, insert, deleteMin, merge) where
+module ExplicitMin where
+  -- (Heap, empty, isEmpty, findMin, insert, deleteMin, merge) where
 
 -- NOTE: without functors or type classes, we would manually swap in
 -- different implementations of H by twiddling the following imports
@@ -7,46 +7,74 @@ module ExplicitMin
 import BinomialHeaps as H
 -- import LeftistHeaps as H
 
-type Heap =
+type Heap a =
        E
-     | NE Int H.Heap   -- the Int is the minimum element
+     | NE Int a   -- the Int is the minimum element
 
-empty : Heap
-empty = E
+type alias HeapFunctions a = {
+    empty : a,
+    isEmpty : a -> Bool,
+    insert : Int -> a -> a,
+    merge : a -> a -> a,
+    findMin : a -> Maybe Int,
+    deleteMin : a -> Maybe a
+  }
 
-isEmpty : Heap -> Bool
-isEmpty heap = case heap of
+binominalFunctions = {
+    empty = H.empty,
+    isEmpty = H.isEmpty,
+    insert = H.insert,
+    merge = H.merge,
+    findMin = H.findMin,
+    deleteMin = H.deleteMin
+  }
+
+wrap : HeapFunctions a -> HeapFunctions (Heap a)
+wrap fs = {
+    empty = empty fs,
+    isEmpty = isEmpty fs,
+    insert = insert fs,
+    merge = merge fs,
+    findMin = findMin fs,
+    deleteMin = deleteMin fs
+  }
+
+empty : HeapFunctions a -> Heap a
+empty fs = E
+
+isEmpty : HeapFunctions a -> Heap a -> Bool
+isEmpty fs heap = case heap of
   E -> True
   _ -> False
 
-insert : Int -> Heap -> Heap
-insert x heap = case heap of
-  E -> NE x <| H.insert x H.empty
+insert : HeapFunctions a -> Int -> Heap a -> Heap a
+insert fs x heap = case heap of
+  E -> NE x <| fs.insert x fs.empty
 
   NE previousMin internalHeap ->
     let newMin = min previousMin x
     in
-       NE newMin <| H.insert x internalHeap
+       NE newMin <| fs.insert x internalHeap
 
-merge : Heap -> Heap -> Heap
-merge heap1 heap2 = case (heap1, heap2) of
+merge : HeapFunctions a -> Heap a -> Heap a -> Heap a
+merge fs heap1 heap2 = case (heap1, heap2) of
   (E, _) -> heap2
   (_, E) -> heap1
 
   (NE min1 internalHeap1, NE min2 internalHeap2) ->
-    NE (min min1 min2) (H.merge internalHeap1 internalHeap2)
+    NE (min min1 min2) (fs.merge internalHeap1 internalHeap2)
 
-findMin : Heap -> Maybe Int
-findMin heap = case heap of
+findMin : HeapFunctions a -> Heap a -> Maybe Int
+findMin fs heap = case heap of
   E -> Nothing
   NE min _ -> Just min
 
-deleteMin : Heap -> Maybe Heap
-deleteMin heap = case heap of
+deleteMin : HeapFunctions a -> Heap a -> Maybe (Heap a)
+deleteMin fs heap = case heap of
   E -> Nothing
   NE _ internalHeap ->
-    let maybeNewHeap = H.deleteMin internalHeap
-        maybeNewMin = Maybe.andThen maybeNewHeap H.findMin
+    let maybeNewHeap = fs.deleteMin internalHeap
+        maybeNewMin = Maybe.andThen maybeNewHeap fs.findMin
     in
        case (maybeNewHeap, maybeNewMin) of
          (Just newHeap, Just newMin) -> NE newMin newHeap |> Just
